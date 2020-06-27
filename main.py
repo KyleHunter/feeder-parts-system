7b201b1f39294c9bfc3a5056ab7e323034cff354
Kyle <Kyle@microtype.io>
2020-06-27 08:13:21 -0400

    Correctly extracts feeder data and exports to CSV

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+
+# PARTS FEEDER SYSTEM
+
 TEXT_FILE_LOCATION = "BBS_PAD_LED_NEW.txt"
-VALIDATION_LINE = 2
-VALIDATION_TEXT = "&I.COMMENT=Philips_EMT_bv"
 
 with open(TEXT_FILE_LOCATION) as file:  # Use file to refer to the file object
     data = file.readlines()
@@ -8,12 +10,13 @@ with open(TEXT_FILE_LOCATION) as file:  # Use file to refer to the file object
 
 # Returns true if we are in a valid VIOS txt file
 def valid_file():
-    return VALIDATION_TEXT in data[VALIDATION_LINE]
+    return "&I.COMMENT=Philips_EMT_bv" in data[2]
 
-# Returns a 2D array of feeder data: [i, j]
-# i is part number
-# j is un formatted lines of the parts feeder info
-def get_feeder_data():
+
+# Returns a 2D array of raw feeder data: [i, j]
+# i is index
+# j is un-formatted lines of the parts feeder info
+def get_raw_feeder_data():
     fd_data = 0
     res = []
     res_internal = []
@@ -37,6 +40,33 @@ def get_feeder_data():
             return res
 
 
-val = get_feeder_data()
-for i in val[0]:
-        print(repr(i))
+# Returns a 2D array of formatted feeder data [i, j]
+# i is index
+# j is [Manf. part no, raw feeder type, feeder slot]
+def extract_feeder_data():
+    raw = get_raw_feeder_data()
+    res = []
+    for i in raw:
+        res_internal = [i[1].split('    ', 1)[0].strip(), i[3][2:4].strip()]
+        f_slot = int(i[5].split('     ')[2].strip())
+        if f_slot < 64:
+            f_slot += 1
+        if f_slot > 64:
+            f_slot += 37
+
+        res_internal.append(str(f_slot))  # Raw feeder slot
+        res.append(res_internal)
+    return res
+
+
+# Writes formatted feeder data to output.csv
+def write_output():
+    with open('output.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["MANF PART NO", "FEEDER TYPE", "FEEDER SLOT"])
+        val = extract_feeder_data()
+        for i in val:
+            writer.writerow(i)
+
+
+write_output()
